c6201d6d152ee5b726a289ddcbd8657252843e90
fexjoo <f.j.grossmann@hotmail.de>
2017-04-04 12:01:03 +0200

    Version 1.0

--- 20170403_Eingabenvalidierung.R
+++ 20170403_Eingabenvalidierung.R
@@ -1,28 +1,29 @@
-library(shiny)
-shinyApp(ui = ui, server = server)
+# Vor Verwendung der App die Datei in "app.R" umbenennen!
 
+library(shiny)
 
 server <- function(input, output) {
   
   output$text_ergebnis_buchstabe <- renderText({
-    # Überprüfung, ob der eingegebene Wert nur Buchstaben enthaelt und nicht leer ist
+    # Rruefung, ob der eingegebene Wert nur Buchstaben enthaelt und nicht leer ist
     validate(
-      need(grepl("^[A-Za-z]+$", input$text_buchstabe, perl = TRUE) & input$text_buchstabe != "", "Bitte Buchstaben eingeben!")
+      need(grepl("^[A-Za-z]+$", input$text_buchstabe, perl = TRUE) & input$text_buchstabe != "", "Bitte Buchstaben eingegeben!")
     )
     paste0("Sie haben erfolgreich die Buchstaben ", input$text_buchstabe, " eingeben!")
   })
   
   output$text_ergebnis_zahl <- renderText({
-    # Überprüfung, ob der eingegebene Wert nur Zahlen enthaelt und nicht leer ist
+    # Pruefung, ob der eingegebene Wert nur Zahlen enthaelt und nicht leer ist
     validate(
-      need(grepl("^[0-9.]+$", input$text_zahl, perl = TRUE) & input$text_zahl != "", "Bitte Zahl eingeben!")
+      need(grepl("^[0-9.]+$", input$text_zahl, perl = TRUE) & input$text_zahl != "", "Bitte Zahl eingegeben!")
     )
     paste0("Sie haben erfolgreich die Zahl ", input$text_zahl, " eingeben!")
   })
   
   output$text_ergebnis_select <- renderText({
+    # Pruefung, ob der SelectInput gefuellt ist
     req(input$select_auswahl)
-    print(paste0("Sie haben erfolgreich die ", input$select_auswahl, " gewählt!\n"))
+    paste0("Sie haben erfolgreich die ", input$select_auswahl, " gewählt!")
   })
 }
 
@@ -33,7 +34,7 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       textInput("text_buchstabe", "Bitte Buchstaben eingeben:"),
-      textInput("text_zahl", "Bitte Zahl eingeben:"),
+      textInput("text_zahl", "Bitte Zahl zw. 1 und 26 eingeben:"),
       selectInput("select_auswahl", "Bitte Option wählen:", choices = c("Option A"), selected = NULL, multiple = TRUE)
       
     ),
@@ -46,3 +47,5 @@ ui <- fluidPage(
     )
   )
 )
+
+shinyApp(ui = ui, server = server)
